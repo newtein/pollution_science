9a6003b3184c74de1eaaa6b031ea94c0eb4e1640
newtein <harshitgujral12@gmail.com>
2020-07-12 04:41:32 +0530

    steps and details

--- gam_model.R
+++ gam_model.R
@@ -1,6 +1,39 @@
 library(mgcv)
-df <- read.csv("D:/CovidPollution/r_files/pollutant_pm2.csv")
-lm(cases~pollutant+mean_pressure+mean_rh+mean_wind+city+date_number+cases_shifted, data=df)
-gam_y <- gam(log(cases)~pollutant+s(mean_pressure)+s(mean_rh)+s(mean_wind)+city+date_number+log(cases_shifted), method = "REML", data=df)
-gam_y
-summary(gam_y)
+
+state_code <- "06"
+pollutants <- c("PM2", "PM10", "O3")
+lag_list <- c("0-7", "0-14", "0-21")
+for (pollutant in pollutants){
+  for (lag_t in lag_list){
+    print(paste(pollutant, lag_t))
+    fname = "D:/CovidPollution/r_files/R_data"
+    wfname = "D:/CovidPollution/r_files/model_summary/gam_summary"
+    
+    cases_fname = paste(fname, pollutant, state_code, lag_t, "COVID_cases.csv", sep="_")
+    deaths_fname = paste(fname, pollutant, state_code, lag_t, "COVID_mortality.csv", sep="_")
+    cases_wfname = paste(wfname, pollutant, state_code, lag_t, "COVID_cases.csv", sep="_")
+    deaths_wfname = paste(wfname, pollutant, state_code, lag_t, "COVID_mortality.csv", sep="_")
+    
+    df_cases <- read.csv(cases_fname)
+    df_deaths <- read.csv(deaths_fname)
+    
+    formula_1 = log(cases)~pollutant+s(mean_pressure)+s(mean_rh)+s(mean_wind)+city+date_number+log(cases_shifted)
+    formula_2 = log(deaths)~pollutant+s(mean_pressure)+s(mean_rh)+s(mean_wind)+city+date_number+log(deaths_shifted)
+    
+    gam_y_cases <- gam(formula = formula_1, method = "REML", data=df_cases)
+    print("CASES")
+    sink(cases_wfname)
+    print(summary(gam_y_cases))
+    sink()
+
+    gam_y_deaths <- gam(formula = formula_2, method = "REML", data=df_deaths)
+    print("DEATHS")
+    sink(deaths_wfname)
+    print(summary(gam_y_deaths))
+    sink()
+
+    print(paste(pollutant, lag_t))
+    
+  }
+}
+
